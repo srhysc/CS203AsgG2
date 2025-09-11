584dac7ad71d9a36b462795036ba0908fcfa4fd5
srhysc <64086762+srhysc@users.noreply.github.com>
2025-09-11 20:14:12 +0800

    Base files Springboot files added

---
+++ cs203g12tariff/src/test/java/sg/edu/smu/cs203/g12/cs203g12tariff/Cs203G12TariffProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.smu.cs203.g12.cs203g12tariff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs203G12TariffProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
