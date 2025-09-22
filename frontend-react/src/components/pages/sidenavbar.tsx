de00553f8ed17c75a7ac82ad323a3513780f7171
yanhan220 <leeyanhan166@gmail.com>
2025-09-22 23:06:30 +0800

    Installed tailwind and shadcn libraries into react app

---
+++ frontend-react/src/components/pages/sidenavbar.tsx
@@ -0,0 +1,57 @@
+// src/components/Layout.tsx
+
+import { Home, Search } from "lucide-react"
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarHeader,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarGroupContent
+} from "@/components/ui/sidebar"
+
+const items = [
+  {
+    title: "Home",
+    url: "#",
+    icon: Home,
+  },
+
+  {
+    title: "Tariff Search",
+    url: "#",
+    icon: Search,
+  },
+
+]
+
+export function AppSideBar(){
+    return(
+        <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+    )
+}
