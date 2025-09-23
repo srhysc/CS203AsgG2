200e17738848bfb85d3417bbe48fcd448a8a2c8b
NoahSau2003 <noah.sau.2024@computing.smu.edu.sg>
2025-09-23 16:31:34 +0800

    Country Codes

--- backend/Asg2/src/main/java/com/cs203/grp2/Asg2/country/Country.java
+++ backend/Asg2/src/main/java/com/cs203/grp2/Asg2/country/Country.java
@@ -1,5 +1,45 @@
+package com.cs203.grp2.Asg2.country;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+
 public class Country {
-    private static String name;
-    private static int ISO6code;
-    
+
+    @NotBlank(message = "Name must not be blank")
+    private String name;
+
+    @Min(value = 1, message = "ISO6code must be positive")
+    private int ISO6code;
+
+    public Country() {
+    }
+
+    public Country(String name, int ISO6code) {
+        this.name = name;
+        this.ISO6code = ISO6code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getISO6code() {
+        return ISO6code;
+    }
+
+    public void setISO6code(int ISO6code) {
+        this.ISO6code = ISO6code;
+    }
+
+    @Override
+    public String toString() {
+        return "Country{" +
+                "name='" + name + '\'' +
+                ", ISO6code=" + ISO6code +
+                '}';
+    }
 }
--- backend/Asg2/src/main/java/com/cs203/grp2/Asg2/country/CountryService.java
+++ backend/Asg2/src/main/java/com/cs203/grp2/Asg2/country/CountryService.java
@@ -1,3 +1,27 @@
+package com.cs203.grp2.Asg2.country;
+
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
 public class CountryService {
-    
+    private final Map<Integer, Country> countries = new HashMap<>();
+
+    public CountryService() {
+        // Pre-load some countries with ISO6code as key
+        countries.put(250, new Country("France", 250));
+        countries.put(276, new Country("Germany", 276));
+    }
+
+    public Country getCountryByCode(int iso6code) {
+        return countries.get(iso6code);
+    }
+
+    public void addCountry(Country country) {
+        countries.put(country.getISO6code(), country);
+    }
+
+    // You can add update/delete methods here as needed
 }
