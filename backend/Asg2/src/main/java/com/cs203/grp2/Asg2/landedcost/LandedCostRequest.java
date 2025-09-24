a48b99a266d23cef6e961c1198966f15a1e7d664
charissa-tan <charissatan.2024@computing.smu.edu.sg>
2025-09-24 16:45:16 +0800

    updated landedcost

--- backend/Asg2/src/main/java/com/cs203/grp2/Asg2/landedcost/LandedCostRequest.java
+++ backend/Asg2/src/main/java/com/cs203/grp2/Asg2/landedcost/LandedCostRequest.java
@@ -1,9 +1,20 @@
 package com.cs203.grp2.Asg2.landedcost;
 
+import jakarta.validation.constraints.*;
+
 public class LandedCostRequest {
+    @NotNull 
+    @Min(1)
     private int importerIso3n;   // numeric ISO code
+
+    @NotNull 
+    @Min(1)
     private int exporterIso3n;   // numeric ISO code
+
+    @Pattern(regexp="\\d{6}", message="HS Code must be 6 digits")
     private String hsCode;
+
+    @Min(1) // enforce a validation constraint on a numeric field, ensuring that its value >= 1
     private int units;
 
     public LandedCostRequest() {}
