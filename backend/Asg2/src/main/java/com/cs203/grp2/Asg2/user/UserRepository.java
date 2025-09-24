fc0b5862ddca52d1b2d0d499135bce43a9aab33c
NoahSau2003 <noah.sau.2024@computing.smu.edu.sg>
2025-09-24 11:25:40 +0800

    vat and landedcost delete

--- backend/Asg2/src/main/java/com/cs203/grp2/Asg2/user/UserRepository.java
+++ backend/Asg2/src/main/java/com/cs203/grp2/Asg2/user/UserRepository.java
@@ -2,6 +2,8 @@ package com.cs203.grp2.Asg2.user;
 
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
