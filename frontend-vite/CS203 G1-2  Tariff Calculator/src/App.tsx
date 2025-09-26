3aab5ccf3b96008908a28714b5ca1de983de5292
yanhan220 <leeyanhan166@gmail.com>
2025-09-26 23:17:31 +0800

    Imported aceternity UI aurora beam package

--- frontend-vite/CS203 G1-2  Tariff Calculator/src/App.tsx
+++ frontend-vite/CS203 G1-2  Tariff Calculator/src/App.tsx
@@ -3,10 +3,23 @@ import { Layout } from '@/components/pages/layout';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import CountryTariffList from '@/services/CountryTariffList';
 import TariffCalculator from './services/TariffCalculator';
+import { AuroraBackground } from './components/ui/aurora-background';
+import {motion} from "motion/react"
 
 function App() {
 
   return (
+    <AuroraBackground>
+      <motion.div
+        initial={{ opacity: 0.0, y: 40 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.3,
+          duration: 0.8,
+          ease: "easeInOut",
+        }}
+        className="relative flex flex-col gap-4 items-center justify-center px-4"
+      >
     <Router>
     <Layout>
       <div className="App">
@@ -34,6 +47,8 @@ function App() {
       </div>
     </Layout>
     </Router>
+    </motion.div>
+    </AuroraBackground>
   );
 }
 
