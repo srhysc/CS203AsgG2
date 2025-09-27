358ca5cc5d282355e2d765826c6526317fe23a78
srhysc <srhyschua@gmail.com>
2025-09-28 02:10:08 +0800

    removed unecessary imports

--- frontend-vite/TariffCalculator/src/services/TariffCalculator.tsx
+++ frontend-vite/TariffCalculator/src/services/TariffCalculator.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState} from 'react';
 import type { tariff } from './counttrytariffapi'; 
 import { tariffService } from './counttrytariffapi';
 import { TariffForm } from '@/components/ui/tarifflookupform';
