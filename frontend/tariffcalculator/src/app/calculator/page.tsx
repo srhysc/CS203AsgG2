853bb97b6be341c5be3a9b358499bfc2b77087cb
yanhan220 <leeyanhan166@gmail.com>
2025-09-27 21:19:31 +0800

    removed extra frontend folder

--- frontend/tariffcalculator/src/app/calculator/page.tsx
+++
@@ -1,9 +0,0 @@
-import TariffCalculator from "./TariffCalculator";
-
-export default function CalculatorPage() {
-  return (
-    <main className="min-h-dvh bg-background text-foreground flex items-center justify-center p-6">
-      <TariffCalculator />
-    </main>
-  );
-}
